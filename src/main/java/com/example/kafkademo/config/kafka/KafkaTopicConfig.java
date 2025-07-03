47690e3dfbec1e1dda711858f65ffeceafd7e2a9
mabattal <mehmetalibattal@gmail.com>
2025-07-03 18:26:58 +0300

    kafkaTopicConfig

--- src/main/java/com/example/kafkademo/config/kafka/KafkaTopicConfig.java
+++ src/main/java/com/example/kafkademo/config/kafka/KafkaTopicConfig.java
@@ -1,16 +1,22 @@
 package com.example.kafkademo.config.kafka;
 
+import com.example.kafkademo.properties.kafka.KafkaProperties;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+
 @Configuration
+@RequiredArgsConstructor
 public class KafkaTopicConfig {
+
+    private final KafkaProperties kafkaProperties;
 
     @Bean
     public NewTopic photoUploadDltTopic() {
-        return TopicBuilder.name("photo-upload.DLT")
+        return TopicBuilder.name(kafkaProperties.getTopic().getPhotoUploadDlt())
                 .partitions(1)
                 .replicas(1)
                 .build();
